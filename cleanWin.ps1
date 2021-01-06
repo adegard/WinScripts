529dd75c554194438e5ca42bd93d6e2b9d2d399f
adegard <degardinarnaud@gmail.com>
2021-01-06 18:36:15 +0100

    Update cleanWin.ps1

--- cleanWin.ps1
+++ cleanWin.ps1
@@ -1,7 +1,7 @@
 ##################################################################################   
 <# 
 based modifyed of the followings
-https://gallery.technet.microsoft.com/scriptcenter/Powershell-script-to-12de6135
+
  https://teckangaroo.com/clean-up-unwanted-files-in-pc/
 
 by adegard, 2021
@@ -9,10 +9,16 @@ https://github.com/adegard/WinScripts/
  
 #> 
 ##################################################################################   
-  (New-Object -ComObject Shell.Application).Namespace(0xA).items() | %{ rm $_.path -Recurse -Confirm:$false}
+ write-Host "Empty recycle bin using Powershell.."
+(New-Object -ComObject Shell.Application).Namespace(0xA).items() | %{ rm $_.path -Recurse -Confirm:$false}
+
+ write-Host "Remove Windows temporary files using PowerShell.."
   rm c:\Windows\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
+  
+ write-Host "Delete User temp files using PowerShell.." 
   rm “C:\Users\*\Appdata\Local\Temp\*” -Recurse -Force -ErrorAction SilentlyContinue
   
+write-Host "Run disk cleanup using PowerShell.."  
 cleanmgr /sagerun:1 | out-Null
         write-Host "**Clean Up completed**" 
 ##End of execution## 
